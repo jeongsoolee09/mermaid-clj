5d173a7eac516a77cf37027e0183a87c328c31ba
이정수 <jeongsoolee@korea.ac.kr>
2022-09-15 12:09:40 -0700

    update

--- src/mermaid_clj/flowchart.clj
+++ src/mermaid_clj/flowchart.clj
@@ -2,34 +2,119 @@
 
 ;; ============ nodes ============
 
-(defn node [])
+(defn node [& message]
+  {:type    :node/normal
+   :message message})
 
-(defn round-edge [])
+(defn round-edge [& message]
+  {:type    :node/round-edge
+   :message message})
 
-(defn pill [])
+(defn pill [& message]
+  {:type    :node/pill
+   :message message})
+
+(defn subroutine [& message]
+  {:type    :node/subroutine
+   :message message})
+
+(defn database [& message]
+  {:type    :node/database
+   :message message})
+
+(defn circle [& message]
+  {:type    :node/circle
+   :message message})
+
+(defn ribbon [& message]
+  {:type    :node/ribbon
+   :message message})
+
+(defn rhombus [& message]
+  {:type    :node/rhombus
+   :message message})
+
+(defn hexagon [& message]
+  {:type    :node/hexagon
+   :message message})
+
+(defn slanted [& message]
+  {:type    :node/slanted
+   :message message})
+
+(defn slanted-alt [& message]
+  {:type    :node/slanted-alt
+   :message message})
+
+(defn trapezoid [& message]
+  {:type    :node/trapezoid
+   :message message})
+
+(defn trapezoid-alt [& message]
+  {:type    :node/trapezoid-alt
+   :message message})
+
+(defn double-circle [& message]
+  {:type    :node/double-circle
+   :message message})
 
 ;; ============ links ============
 
-(defn normal-line [from to & {:keys [length]
-                              :or {length 1}}]
-  )
+(defn line [from to
+            & message {:keys [length]
+                       :or   {length 1}}]
+  {:type    :line/normal
+   :from    from
+   :to      to
+   :message message})
 
-(defn normal-arrow [from to & {:keys [length]
-                               :or   {length 1}}])
+(defn arrow [from to
+             & message {:keys [length]
+                        :or   {length 1}}]
+  {:type    :arrow/normal
+   :from    from
+   :to      to
+   :message message})
 
-(defn thick-line [from to & {:keys [length]
-                             :or   {length 1}}])
+(defn thick-line [from to
+                  & message {:keys [length]
+                             :or   {length 1}}]
+  {:type    :line/thick
+   :from    from
+   :to      to
+   :message message})
 
-(defn thick-arrow [from to & {:keys [length]
-                              :or   {length 1}}])
+(defn thick-arrow [from to
+                   & message {:keys [length]
+                              :or   {length 1}}]
+  {:type    :arrow/thick
+   :from    from
+   :to      to
+   :message message})
 
-(defn dotted-line [from to & {:keys [length]
-                              :or   {length 1}}])
+(defn dotted-line [from to
+                   & message {:keys [length]
+                              :or   {length 1}}]
+  {:type    :line/dotted
+   :from    from
+   :to      to
+   :message message})
 
-(defn dotted-arrow [from to & {:keys [length]
-                               :or   {length 1}}])
+(defn dotted-arrow [from to
+                    & message {:keys [length]
+                               :or   {length 1}}]
+  {:type    :arrow/dotted
+   :from    from
+   :to      to
+   :message message})
 
-;; ============ normal ============
+;; ============ subgraph ============
+
+(defn flowchart [])
+
+;; ============ flowchart ============
+
+(defn flowchart [])
 
 ;; flowchart TD
 ;;     A[Start] --> B{Is it?}
@@ -43,5 +128,14 @@
 ;; => what's the point of using an embedded DSL when you can't use the host language
 ;;    to write the desired software?
 
-(flow-chart :TD
-   (let ))
+;; (flow-chart :TD        ; can either be symbol, keyword, or string
+;;    (let [A (node "Start")
+;;          B (rhombus "Is it?")
+;;          C (node "OK")
+;;          D (node "Rethink")
+;;          E (node "End")]
+;;      (arrow A B)
+;;      (arrow B C "Yes")
+;;      (arrow C D)
+;;      (arrow D B)
+;;      (arrow B E "No")))
