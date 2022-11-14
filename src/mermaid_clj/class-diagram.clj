fff806f1eccba1c405c4e633c1599e8bf891ee14
Jeongsoo Lee <jslee@unknown5eaf974a8380.attlocal.net>
2022-11-14 14:21:35 -0800

    update

--- src/mermaid_clj/class-diagram.clj
+++ src/mermaid_clj/class-diagram.clj
@@ -1,16 +1,16 @@
 (ns mermaid-clj.class-diagram
   (:require [clojure.string :as string]))
 
-(defn type-normalize [type]
+(defn- type-normalize [type]
   (string/replace (name :List<Integer>) #"(<|>)" "~" ))
 
 ;; ================ classes ================
 
-(defn class-builder [visibility]
-  (fn [name & forms]
+(defn- class-builder [visibility]
+  (fn [classname & forms]
     {:type  :class
-     :kind  visibility
-     :id    name
+     :kind  (classname visibility)
+     :id    (name classname)
      :forms forms}))
 
 (def class           (class-builder :public)) ; class defaults to public
@@ -32,25 +32,25 @@
 
 ;; ================ members ================
 
-(defn member-builder [visibility]
+(defn- member-builder [visibility]
   (fn
     ([method-name]
      {:type    :member
-      :kind    visibility
+      :kind    (name visibility)
       :rtntype :void
-      :id      method-name
+      :id      (name method-name)
       :args    []})
     ([method-name args]
      {:type    :member
-      :kind    visibility
+      :kind    (name visibility)
       :rtntype :void
-      :id      method-name
+      :id      (name method-name)
       :args    args})
     ([rtntype method-name args]
      {:type    :member
-      :kind    visibility
+      :kind    (name visibility)
       :rtntype (type-normalize rtntype)
-      :id      method-name
+      :id      (name method-name)
       :args    args})))
 
 (def public    (member-builder :public))
@@ -73,24 +73,34 @@
   ;; available cardinalities:
   ;;     :1 :0..1 :1..* :*
   ;;     and :n :0..n :1..n where n>1 integer
-  {:type         extends
-   :subclass     subclass
-   :superclass   superclass
-   :head         head
-   :direction    direction
-   :relationship relationship
-   :cardinality  cardinality})
+  {:type         :extends
+   :subclass     (name subclass)
+   :superclass   (name superclass)
+   :head         (name head)
+   :direction    (name direction)
+   :relationship (name relationship)
+   :cardinality  (name cardinality)})
 
 ;; ================ render ================
 
-(defn render-class [class])
+(declare render-with-indent)
+(declare dispatch-renderer)
 
-(defn render-member [member])
+(defn- make-indent [indent]
+  (string/join (for [_ (range indent)] " ")))
 
-(defn render-extends [extends])
+(defn- render-class [indent-level class]
+  (let [{:keys [kind id forms]} class
+        indent (make-indent indent-level)]
+    ))
 
-(defn dispatch-renderer [form]
-  (condp = (namespace (form :type))
+(defn- render-member [indent-level member]
+  (let [{:keys [kind rtntype id args]} member]))
+
+(defn- render-extends [indent-level extends])
+
+(defn- dispatch-renderer [form]
+  (condp = (name (form :type))
     "class"   render-class
     "member"  render-member
     "extends" render-extends))
@@ -132,3 +142,7 @@
              (public :bool 'isWild))
            (methods
              (private 'run)))))
+
+(comment
+  (render (public-class :Animal))
+  (name (public-class :Animal) :type))
